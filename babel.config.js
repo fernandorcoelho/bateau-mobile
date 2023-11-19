aa932990a3a1f3be0275f7b928a4c9720f144d8e
Fernando <fernando@dinnisoft.com.br>
2023-11-18 22:00:31 -0300

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
+  const presets = ['babel-preset-expo'];
+
+  const plugins = [
+    ['module-resolver'],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: false
+      }
+    ]
+  ];
+
   return {
-    presets: ['babel-preset-expo']
+    presets,
+    plugins
   };
 };
